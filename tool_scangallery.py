4e4b838a8fdf716ddb39dbbe3bcf4ecd7bb68b12
n0x5 <n0x5@users.noreply.github.com>
2017-07-15 15:59:38 +0200

    added image resolution to db

--- tool_scangallery.py
+++ tool_scangallery.py
@@ -5,14 +5,16 @@ cwd = r'/folder/image/gallery'
 
 conn = sqlite3.connect('images.db')
 cur = conn.cursor()
-cur.execute('''CREATE TABLE images (file text, fullpath text, subfolder text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
+cur.execute('''CREATE TABLE images
+             (file text, fullpath text, subfolder text, size text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
 
 for subdir, dirs, files in os.walk(cwd):
     for fn in files:
         fullpath = os.path.join(subdir, fn)
         subfolder = os.path.basename(os.path.join(subdir))
         if 'thumbs' not in subfolder:
-            print(fullpath)
-            cur.execute('INSERT INTO images (file, fullpath, subfolder) VALUES (?,?,?)', (fn, fullpath, subfolder))
+            imh = Image.open(fullpath).size
+            imh2 = re.sub('[(/:)"]', '', str(imh).replace(', ', 'x'))
+            print(fullpath, imh2)
+            cur.execute('INSERT INTO images (file, fullpath, subfolder, size) VALUES (?,?,?,?)', (fn, fullpath, subfolder, imh2))
             cur.connection.commit()
-
