588ffb2ba71e181428be69dbeba932978bee8e43
n0x5 <n0x5@users.noreply.github.com>
2017-07-17 00:20:11 +0200

    updated with meta and other things

--- tool_scangallery.py
+++ tool_scangallery.py
@@ -1,14 +1,19 @@
 import re
+import time
 import os
-from PIL import Image
+from datetime import datetime
+import calendar
 import sqlite3
+from PIL import Image
+import pyexiv2
 
-cwd = r'/folder/image/gallery'
+
+cwd = r'/image/folder'
 
 conn = sqlite3.connect('images.db')
 cur = conn.cursor()
 cur.execute('''CREATE TABLE images
-             (file text, fullpath text, subfolder text, sizewidth int, sizeheight int, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
+             (file text, fullpath text, subfolder text, sizewidth int, sizeheight int, ftime int, exifd text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
 
 for subdir, dirs, files in os.walk(cwd):
     for fn in files:
@@ -16,8 +21,18 @@ for subdir, dirs, files in os.walk(cwd):
         subfolder = os.path.basename(os.path.join(subdir))
         if 'thumbs' not in subfolder:
             imh = Image.open(fullpath).size
+            ims = str(os.path.getmtime(fullpath)).replace('.0', '')
+            ex2 = pyexiv2.ImageMetadata(fullpath)
+            ex2.read()
+            try:
+                #ex4 = [str(ex2[item].value) for item in ex2.iptc_keys] # for only the value and not key
+                ex4 = [str(ex2[item]) for item in ex2.iptc_keys]
+                ex5 = [str(ex2[item]) for item in ex2.exif_keys]
+                ex6 = ex4, ex5
+            except:
+                pass
             imh2 = re.sub('[(/:)"]', '', str(imh).replace(', ', 'x'))
             swidth = imh2.split('x')
-            print(fullpath, swidth)
-            cur.execute('INSERT INTO images (file, fullpath, subfolder, sizewidth, sizeheight) VALUES (?,?,?,?,?)', (fn, fullpath, subfolder, int(swidth[0]), int(swidth[1])))
+            print(fullpath, swidth, ims, str(ex6))
+            cur.execute('INSERT INTO images (file, fullpath, subfolder, sizewidth, sizeheight, ftime, exifd) VALUES (?,?,?,?,?,?,?)', (fn, fullpath, subfolder, int(swidth[0]), int(swidth[1]), int(ims), str(ex6)))
             cur.connection.commit()
