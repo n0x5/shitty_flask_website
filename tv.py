f6c748275db88c1c4a1c2987037a85a38d95e813
n0x5 <n0x5@users.noreply.github.com>
2022-02-12 14:17:48 +0100

    images test 2

--- tv.py
+++ tv.py
@@ -47,14 +47,15 @@ def wiki_details(search=None, search2=None):
     conn.close()
     return render_template('wiki_details.html', results=results, count=count, search=search, search2=search2)
 
-@app.route("/wiki/<search2>/article/<search>")
-def wiki_article(search=None, search2=None):
+
+@app.route("/wiki/<search2>/article2/<search>")
+def wiki_article2(search=None, search2=None):
     conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'databases', '{}'+'.db').format(search2))
     sql = "select content from {} where title like ?" .format(search2)
     results = [item for item in conn.execute(sql, (search,))]
     count = len(results)
     conn.close()
-    final_string1 = '<title>'+search+'</title>'+'<pre style="word-wrap: break-word; white-space: pre-wrap;">'+str(results[0][0])+'</pre>'
+    final_string1 = '<title>'+search+'</title>'+'<pre style="word-wrap: break-word; white-space: pre-wrap;font-family:time;">'+str(results[0][0])+'</pre>'
     final_string2 = re.sub(r'\[\[(.+?)\]\]', r'<a href="\1">\1</a>', final_string1)
     final_string3 = re.sub(r'\=\=\=\=(.+?)\=\=\=\=', r'<h4 style="display:inline;">\1</h4>', final_string2)
     final_string4 = re.sub(r'\=\=\=(.+?)\=\=\=', r'<h3 style="display:inline;">\1</h3>', final_string3)
@@ -66,4 +67,23 @@ def wiki_article(search=None, search2=None):
     final_string10 = re.sub(r"\*(.+?)\n", r'<li>\1</li>', final_string9)
     final_string11 = re.sub(r'{{(.+?)}}', r'{{\1}}<hr>', final_string10)
     final_string = re.sub(r'<a href="(Category.+?)"', r'<a href="/wiki/{}/category/\1">\1</a>' .format(search2), final_string11)
+    imgsearch = re.findall(r'src="\/static\/\w+\/(.+?)"', str(final_string))
+    lst = []
+    for item_img in imgsearch:
+        conn2 = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'databases', '{}_images.db').format(search2))
+        sql2 = "select url, filename from {}_images where filename like ?" .format(search2)
+        results2 = [item for item in conn2.execute(sql2, (item_img,))]
+        file_url = results2[0][0]
+        filename1 = results2[0][1]
+        lst.append([item_img, filename1, file_url])
+        endpoint = os.path.join(os.path.dirname(__file__), 'static', '{}_images', filename1) .format(search2)
+        endpoint2 = os.path.join(os.path.dirname(__file__), 'static', '{}_images') .format(search2)
+        if not os.path.exists(endpoint2):
+            os.makedirs(endpoint2)
+        if not os.path.exists(endpoint):
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+            r = requests.get(file_url, headers=headers)
+            with open(endpoint, 'wb') as cover_jpg:
+                cover_jpg.write(r.content)
+
     return final_string.replace('.html', '').replace('{{up}}<hr>', '')
