1922c0fe15a659d26944f4ac8d4eae58bfa4d7de
n0x5 <n0x5@users.noreply.github.com>
2022-02-15 13:59:09 +0100

    d

--- tv.py
+++ tv.py
@@ -55,13 +55,36 @@ def wiki_article(search=None, search2=None):
     results = [item for item in conn.execute(sql, (search,))]
     count = len(results)
     conn.close()
-    final_string1 = '<title>'+search+'</title>'+'<pre style="word-wrap: break-word; white-space: pre-wrap;font-family:time;">'+str(results[0][0])+'</pre>'
+    final_string1 = '<title>'+search+'</title>'+'<h1>'+search+'</h1>'+'<pre style="word-wrap: break-word; white-space: pre-wrap;font-family:time;width:700px;">'+str(results[0][0])+'</pre>'
     final_string2 = re.sub(r'\[\[(.+?)\]\]', r'<a href="\1">\1</a>', final_string1)
     final_string3 = re.sub(r'\=\=\=\=(.+?)\=\=\=\=', r'<h4 style="display:inline;">\1</h4>', final_string2)
     final_string4 = re.sub(r'\=\=\=(.+?)\=\=\=', r'<h3 style="display:inline;">\1</h3>', final_string3)
     final_string5 = re.sub(r'\=\=(.+?)\=\=', r'<h2 style="display:inline;">\1</h2>', final_string4)
     final_string6 = re.sub(r"'''(.+?)'''", r'<b>\1</b>', final_string5)
-    final_string7 = final_string6.replace('images/', '/static/{}_images/' .format(search2)).replace('{{cite}}', '').replace('{{Cite}}', '')
+    final_string7 = final_string6.replace('images/', 'https://img.domain111.org/{}_images/' .format(search2)).replace('{{cite}}', '').replace('{{Cite}}', '')
+    final_string8 = re.sub(r"''(.+?)''", r'<b>\1</b>', final_string7)
+    final_string9 = re.sub(r"\{\{dablink(.+?)\}\}", r'<b>\1</b><br>', final_string8)
+    final_string10 = re.sub(r"\*(.+?)\n", r'<li>\1</li>', final_string9)
+    final_string11 = re.sub(r'{{(.+?)}}', r'{{\1}}<hr>', final_string10)
+    final_string = re.sub(r'<a href="(Category.+?)"', r'<a href="/wiki/{}/category/\1">\1</a>' .format(search2), final_string11)
+    return final_string.replace('.html', '').replace('{{up}}<hr>', '')
+
+
+
+@app.route("/wiki/<search2>/article2/<search>")
+def wiki_article2(search=None, search2=None):
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'databases', '{}'+'.db').format(search2))
+    sql = "select content from {} where title like ?" .format(search2)
+    results = [item for item in conn.execute(sql, (search,))]
+    count = len(results)
+    conn.close()
+    final_string1 = '<title>'+search+'</title>'+'<h1>'+search+'</h1>'+'<pre style="word-wrap: break-word; white-space: pre-wrap;font-family:time;">'+str(results[0][0])+'</pre>'
+    final_string2 = re.sub(r'\[\[(.+?)\]\]', r'<a href="\1">\1</a>', final_string1)
+    final_string3 = re.sub(r'\=\=\=\=(.+?)\=\=\=\=', r'<h4 style="display:inline;">\1</h4>', final_string2)
+    final_string4 = re.sub(r'\=\=\=(.+?)\=\=\=', r'<h3 style="display:inline;">\1</h3>', final_string3)
+    final_string5 = re.sub(r'\=\=(.+?)\=\=', r'<h2 style="display:inline;">\1</h2>', final_string4)
+    final_string6 = re.sub(r"'''(.+?)'''", r'<b>\1</b>', final_string5)
+    final_string7 = final_string6.replace('images/', 'https://img.domain111.org/{}_images/' .format(search2)).replace('{{cite}}', '').replace('{{Cite}}', '')
     final_string8 = re.sub(r"''(.+?)''", r'<b>\1</b>', final_string7)
     final_string9 = re.sub(r"\{\{dablink(.+?)\}\}", r'<b>\1</b><br>', final_string8)
     final_string10 = re.sub(r"\*(.+?)\n", r'<li>\1</li>', final_string9)
@@ -86,6 +109,7 @@ def wiki_article(search=None, search2=None):
                 r = requests.get(file_url, headers=headers)
                 with open(endpoint, 'wb') as cover_jpg:
                     cover_jpg.write(r.content)
+
 
         return final_string.replace('.html', '').replace('{{up}}<hr>', '')
 
