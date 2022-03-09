a2decc1702239250506d46fa82fd9429516269c6
n0x5 <n0x5@users.noreply.github.com>
2022-03-09 20:30:46 +0100

    discogs test

---
+++ tools/3discogs_releases.py
@@ -0,0 +1,115 @@
+import os
+import re
+from tqdm import tqdm
+import sqlite3
+
+
+sql_db = os.path.join(os.path.dirname( __file__ ), 'discogs-releases.db')
+conn = sqlite3.connect(sql_db)
+cur = conn.cursor()
+cur.execute('''CREATE TABLE if not exists discogs_releases
+        (id text unique, country text, released text,
+        artist_id text, genres text, styles text, title text, label_name text, catno text, label_id text, format_name text,
+        format_qty text, data_quality text, notes text, track_p text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
+
+
+xmlfile = 'discogs_20220301_releases.xml'
+
+def read1():
+    return xml.read(161061)
+
+with open(xmlfile, 'r', encoding="utf=8") as xml:
+    for fname in iter(read1, ''):
+        labels = re.findall(r'<release id="(\d+)" status="(.+?)">(.+?)<\/release>', fname)
+        for item in tqdm(labels):
+            try:
+                id = item[0]
+                try:
+                    country = re.search(r'<country>(.+?)<\/country>', str(item))
+                    country = country.group(1)
+                except:
+                    country = 'None'
+                try:
+                    label = re.search(r'<label name="(.+?)" catno="(.+?)" id="(.+?)"\/>', str(item))
+                    try:
+                        label_name = label.group(1)
+                    except:
+                        label_name = 'None'
+                    try:
+                        catno = label.group(2)
+                    except:
+                        catno = 'None'
+                    try:
+                        label_id = label.group(3)
+                    except:
+                        label_id = 'None'
+                except:
+                    label = 'None'
+                try:
+                    artist_id = re.findall(r'<artist><id>(.+?)<\/id><name>(.+?)<\/name>', str(item))
+                except:
+                    artist_id = 'none'
+                try:
+                    genres = re.findall(r'<genre>(.+?)<\/genre>', str(item))
+                except:
+                    genres = 'None'
+                try:
+                    styles = re.findall(r'<style>(.+?)<\/style>', str(item))
+                except:
+                    styles = 'None'
+                try:
+                    year = re.search(r'<year>(.+?)<\/year>', str(item))
+                    year = year.group(1)
+                except:
+                    year = 'None'
+                try:
+                    title = re.search(r'<title>(.+?)<\/title>', str(item))
+
+                    title = title.group(1)
+                except:
+                    title = 'None'
+                try:
+                    data_quality = re.search(r'<data_quality>(.+?)<\/data_quality>', str(item))
+                    data_quality = data_quality.group(1)
+                except:
+                    data_quality = 'None'
+                try:
+                    formats = re.search(r'<format name="(.+?)" qty="(\d{0,5}?)"', str(item))
+                    try:
+                        format_name = formats.group(1)
+                    except:
+                        format_name = 'None'
+                    try:
+                        format_qty = formats.group(2)
+                    except:
+                        format_qty = 'None'
+                except:
+                    formats = 'None'
+                try:
+                    released = re.search(r'<released>(.+?)<\/released>', str(item))
+                    released = released.group(1)
+                except:
+                    released = 'None'
+                try:
+                    notes = re.search(r'<notes>(.+?)<\/notes>', str(item))
+                    notes = notes.group(1)
+                except:
+                    notes = 'None'
+                try:
+                    tracklist = re.search(r'<tracklist>(.+?)<\/tracklist>', str(item))
+                    tracks = re.findall(r'<track>(.+?)<\/track>', str(tracklist.group(1)))
+                    try:
+                        track_p = re.findall(r'<position>(.+?)<\/position><title>(.+?)<\/title><duration>(.+?)<\/duration>', str(tracks))
+                    except:
+                        track_p = 'None'
+
+                except:
+                    tracklist = 'None'
+                stuff = id, country, str(released), str(artist_id), ', '.join(genres), ', '.join(styles), title, label_name, catno, label_id, format_name, format_qty, data_quality, notes, str(track_p)
+                cur.execute('insert into discogs_releases (id, country, released, artist_id, genres, styles, title, label_name, catno,\
+                            label_id, format_name, format_qty, data_quality, notes, track_p) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (stuff))
+                cur.connection.commit()
+                #print(stuff)
+            except Exception as e:
+                print(e)
+
