8021c95b9d79475a80485616c9842d8c472f8ee8
n0x5 <n0x5@users.noreply.github.com>
2022-08-05 18:32:26 +0200

    #4

--- tools/wikipedia2sql.py
+++ tools/wikipedia2sql.py
@@ -1,42 +1,35 @@
-import xml.etree.ElementTree as ET
-import time
-import re
-import sqlite3
 import os
+import re
 from tqdm import tqdm
+import sqlite3
+from lxml import etree
+import time
 
-xml_f = open('enwiki-20220801-pages-meta-current.xml', encoding='utf-8')
-
-sql_db = os.path.join(os.path.dirname( __file__ ), 'wiki-en.db')
+sql_db = os.path.join(os.path.dirname( __file__ ), 'wiki-en2.db')
 conn = sqlite3.connect(sql_db)
 cur = conn.cursor()
 cur.execute('''CREATE TABLE if not exists wiki
-        (title text, content text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
+        (title text unique, content text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
 
-nodes = iter(ET.iterparse(xml_f, events=['end']))
-_, root = next(nodes)
+xmlfile = 'enwiki-20220801-pages-meta-current.xml'
 
-
-for event, elem in tqdm(nodes):
-    tree = ET.tostring(elem).decode()
-    if ('<ns0:ns>0</ns0:ns>' in tree or '<ns0:ns>14</ns0:ns>' in tree) and '<ns0:redirect title' not in tree:
+context = etree.iterparse(xmlfile, tag='{http://www.mediawiki.org/xml/export-0.10/}page')
+for event, elem in tqdm(context):
+    tree = etree.tostring(elem).decode()
+    if ('<ns>0</ns>' in tree or '<ns>14</ns>' in tree) and '#REDIRECT' not in tree:
         try:
-            title1 = re.search('<ns0:title>(.+)<\/ns0:title>', str(tree), flags=re.DOTALL)
-            text = re.search('<ns0:text .+">(.+)<\/ns0:text>', str(tree), flags=re.DOTALL)
+            title1 = re.search('<title>(.+)<\/title>', str(tree), flags=re.DOTALL)
+            text = re.search('<text .*">(.+)<\/text>', str(tree), flags=re.DOTALL)
             title = title1.group(1)
             content = text.group(1)
             stuff = title, content
-            cur.execute('select exists(select 1 from wiki where title = ? limit 1)', (title,))
-            record = cur.fetchone()
-            if record[0] == 1:
-                pass
-            else:
-                cur.execute('insert into wiki (title, content) values (?,?)', (stuff))
-                cur.connection.commit()
+            cur.execute('insert or ignore into wiki (title, content) VALUES (?,?)', (stuff))
+            cur.connection.commit()
         except Exception:
             print('skipping')
+
         elem.clear()
-
-    root.clear()
-
-xml_f.close()
+    for ancestor in elem.xpath('ancestor-or-self::*'):
+        while ancestor.getprevious() is not None:
+            del ancestor.getparent()[0]
+del context
