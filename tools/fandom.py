50f3fa6df349368c8091f01305ca70307f09720a
n0x5 <n0x5@users.noreply.github.com>
2022-09-21 19:32:26 +0200

    Update fandom.py

--- tools/fandom.py
+++ tools/fandom.py
@@ -5,18 +5,20 @@ import sqlite3
 from lxml import etree
 import time
 
-sql_db = os.path.join(os.path.dirname( __file__ ), 'residentevil.db')
+wiki_name = 'cryptidz'
+
+sql_db = os.path.join(os.path.dirname( __file__ ), '{}.db' .format(wiki_name))
 conn = sqlite3.connect(sql_db)
 cur = conn.cursor()
-cur.execute('''CREATE TABLE if not exists wiki
-        (title text unique, content text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
+cur.execute('''CREATE TABLE if not exists {}
+        (title text unique, content text, dated datetime DEFAULT CURRENT_TIMESTAMP)''' .format(wiki_name))
 
-xmlfile = 'residentevil_pages_current.xml'
+xmlfile = 'cryptidzwikiacom-20180621-history.xml'
 
 context = etree.iterparse(xmlfile, tag='{http://www.mediawiki.org/xml/export-0.10/}page')
 lst = []
 for event, elem in tqdm(context):
-    tree = etree.tostring(elem).decode()
+    tree = etree.tostring(elem, encoding='utf-8').decode()
     if ('<ns>0</ns>' in tree or '<ns>14</ns>' in tree or '<ns>10</ns>' in tree) and '#redirect' not in tree.lower():
         try:
             title1 = re.search('<title>(.+)<\/title>', str(tree), flags=re.DOTALL)
@@ -29,7 +31,7 @@ for event, elem in tqdm(context):
             stuff = title, content
             lst.append(stuff)
             if len(lst) == 2000:
-                cur.executemany('insert or ignore into wiki (title, content) VALUES (?,?)', (lst))
+                cur.executemany('insert or ignore into {} (title, content) VALUES (?,?)' .format(wiki_name), (lst))
                 cur.connection.commit()
                 lst = []
         except Exception as e:
@@ -41,5 +43,5 @@ for event, elem in tqdm(context):
             del ancestor.getparent()[0]
 del context
 
-cur.executemany('insert or ignore into wiki (title, content) VALUES (?,?)', (lst))
+cur.executemany('insert or ignore into {} (title, content) VALUES (?,?)' .format(wiki_name), (lst))
 cur.connection.commit()
