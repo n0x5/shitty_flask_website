e756a9cedfde1f4cadb048a4e8e03559376f9310
n0x5 <n0x5@users.noreply.github.com>
2024-03-06 09:54:26 +0100

    Update tv_imdb.py

--- tools/tv_imdb.py
+++ tools/tv_imdb.py
@@ -40,46 +40,49 @@ headers = {
 }
 
 grab1 = GrabIt()
-url2 = r'https://www.imdb.com/title/{}' .format(args.imdb_id)
+url2 = r'https://www.imdb.com/title/{}/episodes?season={}' .format(args.imdb_id, args.season)
 response2 = requests.get(url2, headers=headers)
 soup3 = BeautifulSoup(response2.text, "html.parser")
-table = soup3.find('script', type=re.compile(r'ld\+json'))
+with open('sorc.txt', 'w') as cover_jpg2:
+    cover_jpg2.write(response2.text)
+table = soup3.find('script', type=re.compile(r'application\/json'))
 try:
     data = json.loads(table.string)
 except:
     data = json.loads(table.get_text())
-show_title = data['name']
 
-endpoint4 = os.path.join(os.path.dirname(__file__), '..', 'cover_tv', args.imdb_id+'.jpg')
-if not os.path.isfile(endpoint4):
-   
-    cover = data['image']
-    id2 = re.search(r'(tt\d+)', str(url2))
-    endpoint = os.path.join(os.path.dirname(__file__), '..', 'cover_tv', id2.group(1)+'.jpg')
-    if not os.path.exists('cover_tv'):
-        os.makedirs('cover_tv')
-    if os.path.isfile(endpoint):
-        print('file exists - skipping')
-    r = requests.get(cover, headers=headers)
-    fn = os.path.basename(cover)
+try:
+    show_title = data['name']
+except:
+    show_title = ''
+try:
+    endpoint4 = os.path.join(os.path.dirname(__file__), '..', 'static', 'cover_tv', args.imdb_id+'.jpg')
+    if not os.path.isfile(endpoint4):
+       
+        cover = data['image']
+        id2 = re.search(r'(tt\d+)', str(url2))
+        endpoint = os.path.join(os.path.dirname(__file__), '..', 'static', 'cover_tv', id2.group(1)+'.jpg')
+        if not os.path.exists('cover_tv'):
+            os.makedirs('cover_tv')
+        if os.path.isfile(endpoint):
+            print('file exists - skipping')
+        r = requests.get(cover, headers=headers)
+        fn = os.path.basename(cover)
 
-    with open(endpoint, 'wb') as cover_jpg:
-        cover_jpg.write(r.content)
+        with open(endpoint, 'wb') as cover_jpg:
+            cover_jpg.write(r.content)
+except:
+    pass
 
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
-
-
-table = soup.find('div', attrs={'class': 'list detail eplist'})
-
-for item in soup.find_all('div', attrs={'class': 'list_item'}):
-    episode_number = item.find('meta', content=re.compile('\d{1,5}'))
-    airdate = item.find('div', attrs={'class': 'airdate'})
-    episode_title = item.find('strong')
-    episode_summary = item.find('div', attrs={'class': 'item_description'})
-    sql = args.imdb_id, show_title.strip(), episode_title.get_text().strip(), airdate.get_text().strip(), \
-          episode_summary.get_text().strip(), args.season, episode_number['content']
+for item in data['props']['pageProps']['contentData']['section']['episodes']['items']:
+    show_title = 'Mission Impossible (1988)'
+    episode_number = item['episode']
+    episode_title = item['titleText']
+    episode_summary = item['plot']
+    season_number = item['season']
+    airdate = str(item['releaseDate']['day'])+' '+str(item['releaseDate']['month'])+' '+str(item['releaseDate']['year'])
+    sql = args.imdb_id, show_title.strip(), episode_title, airdate, episode_summary, season_number, episode_number
     print(sql)
     cur.execute('INSERT INTO tv (imdb_id, show_title, episode_title, air_date, episode_summary, season_number, episode_number) VALUES (?,?,?,?,?,?,?)', (sql))
     cur.connection.commit()
